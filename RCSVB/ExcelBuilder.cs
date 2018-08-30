4d08ac443be30a463d042f3e2d6842709a3cd275
Connor Eaves <42850676+ceaves-thechurch@users.noreply.github.com>
2018-08-30 16:47:50 -0500

    Initial Commit

---
+++ RCSVB/ExcelBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCSVB
+{
+    class ExcelBuilder
+    {
+
+        public static void CreateWorkbook(string source, string destination)
+        {
+            if (string.IsNullOrWhiteSpace (destination))
+            {
+                System.Windows.MessageBox.Show("Please select a valid output path.");
+                return;
+            }
+
+            var excelApp = new Application();
+
+            if (excelApp == null)
+            {
+                System.Windows.MessageBox.Show("Excel is not installed on your system.");
+                return;
+            }
+
+            var excelWorkbook = excelApp.Workbooks.Add();
+
+            var excelWorksheet = (Worksheet)excelWorkbook.Worksheets.get_Item(1);
+
+            excelWorkbook.SaveAs(destination);
+        }
+    }
+}
---
+++ RCSVB/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RCSVB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Realms_CSV_File_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            openFileDialog.InitialDirectory = @"C:\";
+            openFileDialog.Title = "Please select a CSV file exported from Realm";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Realms_CSV_File_TextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Output_XLSX_File_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                Output_XLSX_File_TextBox.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void Process_Realms_CSV_File_to_Output_File_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelBuilder.CreateWorkbook(Realms_CSV_File_TextBox.Text, Output_XLSX_File_TextBox.Text);
+        }
+    }
+}
